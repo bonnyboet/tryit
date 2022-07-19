bf10203e244dadb6fa7f284ee16e59466a45128c
Ethan Tadokoro <etado1408@gmail.com>
2022-07-19 21:03:11 +0900

    added faker gem and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do |i|
+  email = Faker::Internet.email
+  password = Faker::String.random(length: 8)
+  i = User.create(email: email, password: password)
+  id = i.id
+  name = Faker::Name.unique.name
+  description = Faker::Quote.jack_handey
+  category = Faker::Team.sport
+  integer = Faker::Number.decimal_part(digits: 2)
+  boolean = Faker::Boolean.boolean
+  EquipmentListing.create(name: name, description: description, category: category, hourly_rate: integer, available: boolean, user_id: id)
+end
