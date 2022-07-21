24beaa7c7212d6ebb4f51344012dabe27994958c
Ethan Tadokoro <etado1408@gmail.com>
2022-07-21 19:17:21 +0900

    updated equipments

--- app/controllers/equipment_listings_controller.rb
+++ app/controllers/equipment_listings_controller.rb
@@ -1,32 +1,31 @@
 class EquipmentListingsController < ApplicationController
   def index
-    @equipments = EquipmentListing.all
+    @equipment_listings = EquipmentListing.all
   end
 
   def show
-    @equipment = EquipmentListing.find(params[:id])
+    @equipment_listing = EquipmentListing.find(params[:id])
   end
 
   def new
-    @equipment = EquipmentListing.find(params[:equipment_id])
+    @equipment_listing = EquipmentListing.find(params[:equipment_id])
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @equipment = EquipmentListing.find(params[:equipment_id])
+    @equipment_listing = EquipmentListing.find(params[:equipment_id])
     @booking.equipment = @equipment
     @booking.save
-    @restaurant.save
     redirect_to equipment_path(@equipment)
   end
 
   def update
-    @equipment = EquipmentListing.find(params[:equipment_id])
+    @equipment_listing = EquipmentListing.find(params[:equipment_id])
   end
 
   def edit
-    @equipment = EquipmentListing.find(params[:equipment_id])
+    @equipment_listing = EquipmentListing.find(params[:equipment_id])
   end
 
   private
