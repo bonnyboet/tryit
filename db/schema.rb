f19cfdcf0f714be338ec1a0b0f6f51ac2bcc9d35
Payla Vargas <miapaylagvargas@gmail.com>
2022-07-21 21:32:28 +0900

    add address, date, start_time and end_time to the EquipmentListing model and update the validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_104157) do
+ActiveRecord::Schema.define(version: 2022_07_21_122158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 2022_07_19_104157) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.date "date"
+    t.time "start_time"
+    t.time "end_time"
     t.index ["user_id"], name: "index_equipment_listings_on_user_id"
   end
 
