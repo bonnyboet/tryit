916dd7285f1d22aff9f603f313df87abb27ae81b
Payla Vargas <miapaylagvargas@gmail.com>
2022-07-30 10:49:34 +0900

    add more seeds with photos of the listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,7 +82,6 @@ if EquipmentListing.count.zero?
   file3 = URI.open('https://images.unsplash.com/photo-1611251135345-18c56206b863?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
   listing3.photo.attach(io: file3, filename: 'user.png', content_type: 'image/png')
 
-
   user4 = User.create!(
     email: Faker::Internet.email,
     password: Faker::String.random(length: 8)
@@ -99,47 +98,58 @@ if EquipmentListing.count.zero?
   file4 = URI.open('https://images.unsplash.com/photo-1514989940723-e8e51635b782?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')
   listing4.photo.attach(io: file4, filename: 'user.png', content_type: 'image/png')
 
+  user5 = User.create!(
+    email: Faker::Internet.email,
+    password: Faker::String.random(length: 8)
+  )
+  listing5 = user5.equipment_listings.create!(
+    name: "Beginner-friendly Surfboard",
+    description: "For beginner surfers, this 8 foot foam longboard surfboard is the best. This is a foam surfboard is stable, user-friendly, and easy to paddle which makes catching waves and standing up a lot easier.",
+    category: "Aquatic Sports",
+    hourly_rate: 30,
+    available: true,
+    user_id: User.last.id,
+    address: "Enoshima"
+  )
+  file5 = URI.open('https://images.unsplash.com/photo-1537519646099-335112f03225?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')
+  listing5.photo.attach(io: file5, filename: 'user.png', content_type: 'image/png')
 
-# User.create!(
-#   email: Faker::Internet.email,
-#   password: Faker::String.random(length: 8)
-# ).equipment_listings.create!(
-#   name: "Beginner-friendly Surfboard",
-#   description: "For beginner surfers, this 8 foot foam longboard surfboard is the best. This is a foam surfboard is stable, user-friendly, and easy to paddle which makes catching waves and standing up a lot easier.",
-#   category: "Aquatic Sports",
-#   hourly_rate: 30,
-#   available: true,
-#   user_id: User.last.id,
-#   address: "Enoshima"
-# )
+  user6 = User.create!(
+    email: Faker::Internet.email,
+    password: Faker::String.random(length: 8)
+  )
+  listing6 = user6.equipment_listings.create!(
+    name: "2 pairs of roller blades",
+    description: "5 yr old blades but good as new. I know what you're thinking. We aren't in the 80s or 90s. Who'd want to rollerblade? But hey did you know it is one of the most efficient cardiovascular activities you can do in terms of burning the most calories? Try it!",
+    category: "Others",
+    hourly_rate: 10,
+    available: true,
+    user_id: User.last.id,
+    address: "Shinjuku"
+  )
+  file6 = URI.open('https://images.unsplash.com/photo-1578057183197-423269e14014?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')
+  listing6.photo.attach(io: file6, filename: 'user.png', content_type: 'image/png')
 
-# User.create!(
-#   email: Faker::Internet.email,
-#   password: Faker::String.random(length: 8)
-# ).equipment_listings.create!(
-#   name: "2 pairs of roller blades",
-#   description: "5 year old roller blades but good as new. I know what you're thinking. We aren't in the 80s or 90s. Who would want to rollerblade? But hey did you know rollerblading is one of the most efficient cardiovascular activities you can do in terms of burning the most calories? Try it!",
-#   category: "Others",
-#   hourly_rate: 10,
-#   available: true,
-#   user_id: User.last.id,
-#   address: "Shinjuku"
-# )
 
-# User.create!(
-#   email: Faker::Internet.email,
-#   password: Faker::String.random(length: 8)
-# ).equipment_listings.create!(
-#   name: "Snowboard",
-#   description: "All-mountain snowboard designed to work well in all snow conditions and terrain. If you’re just getting started or are unsure of exactly what you need, this all-mountain snowboard is a great choice.",
-#   category: "Snow Sports",
-#   hourly_rate: 20,
-#   available: true,
-#   user_id: User.last.id,
-#   address: "Sapporo"
-# )
+  user7 = User.create!(
+    email: Faker::Internet.email,
+    password: Faker::String.random(length: 8)
+  )
+  listing7 = user7.equipment_listings.create!(
+    name: "Snowboard",
+    description: "All-mountain snowboard designed to work well in all snow conditions and terrain. If you’re just getting started or are unsure of exactly what you need, this all-mountain snowboard is a great choice.",
+    category: "Snow Sports",
+    hourly_rate: 20,
+    available: true,
+    user_id: User.last.id,
+    address: "Sapporo"
+  )
 
-# User.create!(
+  file7 = URI.open('https://images.unsplash.com/photo-1612450622914-59810e84c143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+    ')
+  listing7.photo.attach(io: file7, filename: 'user.png', content_type: 'image/png')
+
+  # User.create!(
 #   email: Faker::Internet.email,
 #   password: Faker::String.random(length: 8)
 # ).equipment_listings.create!(
